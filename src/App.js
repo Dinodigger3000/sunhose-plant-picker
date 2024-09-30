e7030fb5686a76a1b607a871a22e13b78abb1f36
Royce <94499150+Dinodigger3000@users.noreply.github.com>
2024-09-30 13:13:32 -0500

    react app works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Canvas, useFrame , useThree} from '@react-three/fiber';
+
+function Box() {
+  const meshRef = useRef(null);
+  const { viewport } = useThree();
+  const [position, setPosition] = useState([]);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {if (active)
+    meshRef.current.rotation.x += delta;});
+  useEffect(() => {
+    var randomX = (Math.random() - 0.5) * viewport.width;
+    var randomY = (Math.random() - 0.5) * viewport.height;
+    setPosition([randomX, randomY, 0]);}, [viewport]);
+    
+  return (
+    < mesh 
+      ref={meshRef}
+      position={position}
+      scale={hovered ? 1.5 : 1}
+      onClick={(event)=> setActive(!active)}
+      onPointerOver={(event)=>setHover(true)}
+      onPointerOut={(event)=>setHover(false)}>
+      
+      
+      < boxGeometry args={[1, 1, 1]} />
+      < meshStandardMaterial color={active ? 'orange' : 'gray'} />
+    </mesh >
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Canvas style={{ backgroundColor: 'lightblue', height: '100vh'}}>
+          <ambientLight intensity={Math.PI / 2}/>
+          <pointLight position={[0, 10, -10]} decay={0} intensity={Math.PI / 2} />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </Canvas>
+  )
 }
 
 export default App;
