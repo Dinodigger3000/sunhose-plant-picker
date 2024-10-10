4a3eb34f4a6c070c35d53aa55d25e3d06337605a
Royce <94499150+Dinodigger3000@users.noreply.github.com>
2024-10-10 00:56:57 -0500

    it works again somehow! the code is a mess but i need sleep

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Card, ProgressBar } from 'react-bootstrap';
 
 function PlantCard({ plant, onClick }) {
+  const [imageUrl, setImageUrl] = React.useState('');
+
+  React.useEffect(() => {
+    plant.data.imageUrl.then(url => setImageUrl(url));
+  }, [plant.data.imageUrl]);
   return (
     <Card onClick={() => onClick(plant)} style={{ cursor: "pointer" }}>
-      <Card.Img variant="top" src={plant.imageUrl} alt={plant.title} />
+      <Card.Img variant="top" src={imageUrl} alt={plant.data.title} />
       <Card.Body>
-        <Card.Title>{plant.title}</Card.Title>
+        <Card.Title>{plant.data.title}</Card.Title>
         <ProgressBar now={plant.matchPercentage} variant="success" />
         <Card.Text className="percent-match-label">
           {plant.matchPercentage}% Match
--- src/components/PlantGrid.js
+++ src/components/PlantGrid.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import PlantCard from './PlantCard';
 
-function PlantGrid({ plantData, handleCardClick }) {
+function PlantGrid({ plantScores, handleCardClick }) {
+  
+  const [scores, setPlantScores] = React.useState(plantScores);
+  useEffect(() => {
+    async function getPlantScores() {
+      const plantProfiles = (await plantScores);
+    setPlantScores(plantProfiles);
+    }
+    
+    getPlantScores();
+    console.log(scores);
+  }, [plantScores]);
+  if (!scores) {
+    return null;
+  }
+
   return (
     <Row style={{ margin: 0 }}>
-      {plantData.map((plant) => (
+      {scores.map((plant) => (
         <Col xs={12} sm={6} lg={3} className="p-1" key={plant.id}>
           <PlantCard plant={plant} onClick={handleCardClick} />
         </Col>
